3ff8a89a35c157154c021881b47c6dda0b33944a
lifer_098 <02210mat@gmail.com>
2025-10-26 07:25:36 -0400

    Implemented, password system.

--- CommandPromptMotionInput/CommandPromptMotionInput.cpp
+++ CommandPromptMotionInput/CommandPromptMotionInput.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <unordered_map>
-
+#include <string>
 #include "olcConsoleGameEngine.h"
 #include "InputHandler.h"
 
-#define MOTIONS_1_SIZE 27
+#define MOTIONS_1_SIZE 4
+#define MOTIONS_5_SIZE 1
 
 template <std::size_t N>
 struct MotionInput {
@@ -24,6 +25,15 @@ struct MotionInput {
 				buttonMap[name] = true;
 			}
 		}
+	}
+
+	void resetCommands() {
+		for (std::size_t i = 0; i < N; ++i) {
+			validCommands[i].commandTimer = 0;
+			validCommands[i].commandNumber = 0;
+			validCommands[i].executeTimer = 0;
+		}
+
 	}
 
 };
@@ -87,8 +97,19 @@ public:
 	}
 
 protected:
+
+	void clearCommands() {
+		for (std::size_t i = 0; i < MOTIONS_1_SIZE; ++i) {
+			_motions1[i].resetCommands();
+		}
+		for (std::size_t i = 0; i < MOTIONS_5_SIZE; ++i) {
+			_motions5[i].resetCommands();
+		}
+	}
+
 	virtual bool OnUserCreate()
 	{
+		input_Count = 0;
 		m_fPlayerX = 10.0f;
 		m_fPlayerY = 10.0f;
 		tick = 0;
@@ -150,38 +171,90 @@ protected:
 				buttonMap[key] = false;
 		}
 
-		for (std::size_t i = 0; i < 4; ++i) {
+		for (std::size_t i = 0; i < MOTIONS_1_SIZE; ++i) {
 			_motions1[i].checkValidCommands(buttonMap, &_handler);
 		}
-		for (std::size_t i = 0; i < 1; ++i) {
+		for (std::size_t i = 0; i < MOTIONS_5_SIZE; ++i) {
 			_motions5[i].checkValidCommands(buttonMap, &_handler);
 		}
 		//_motions1[1].checkValidCommands(buttonMap, &_handler);
+		std::vector<std::string> possibleInputs = {
+			"INPUT_632147896", "INPUT_412369874", "INPUT_63214", "INPUT_41236", "INPUT_623", "INPUT_421",
+			"INPUT_214",  "INPUT_236",
+			 
+		};
+		std::vector<std::string> possibleButton = {
+			"INPUT_PRESS_LP", "INPUT_PRESS_MP", "INPUT_PRESS_HP",
+			"INPUT_PRESS_LK", "INPUT_PRESS_MK", "INPUT_PRESS_HK"
+		};
 
-
-		if (buttonMap["INPUT_632146"]) {
-			if (buttonMap["INPUT_PRESS_HP"]) {
-				m_fPlayerX += 2.0f;
+		for (std::size_t i = 0; i < 6; ++i) {
+			if (buttonMap[possibleButton[i]]) {
+				for (std::size_t j = 0; j < 8; ++j) {
+					if (buttonMap[possibleInputs[j]]) {
+						m_fPlayerX += 2.0f;
+						commandList.push_back(possibleInputs[j]);
+						buttonList.push_back(possibleButton[i]);
+						clearCommands();
+						++input_Count;
+						break;
+					}
+				}
 			}
 		}
+
+
 
 		if (m_keys[0x52].bHeld) {
 			m_fPlayerX = 0.0f;
 		}
 
 
-		Fill(0, 0, m_nScreenWidth, m_nScreenHeight, PIXEL_SOLID,  inputs % 16);
-
+		Fill(0, 0, m_nScreenWidth, m_nScreenHeight, PIXEL_SOLID, inputs % 16);
+		std::string var = ". . . .";
 		Fill((int)m_fPlayerX, (int)m_fPlayerY, (int)m_fPlayerX + 5, (int)m_fPlayerY + 5, PIXEL_SOLID, 1);
+		/*for (std::size_t i = input_Count; i > -1; i--) {
+			FillCircle((int)m_fPlayerX, (int)m_fPlayerY, 10, PIXEL_SOLID, 1);
+		}*/
 
+		bool matching = helperFunction();
+		
+		std::string passwordDisplay = "";
+		passwordDisplay.reserve(input_Count);
+		for (std::size_t i = 0; i < input_Count; i++) {
+			if (matching) {
+				passwordDisplay.append("YES");
+			}
+			passwordDisplay.append(commandList[input_Count-1]);
+		}
+		std::wstring wsTmp(passwordDisplay.begin(), passwordDisplay.end());
+		std::wstring wideString = std::to_wstring(input_Count);
+		DrawString((int)m_fPlayerX, (int)m_fPlayerY, wsTmp, 1);
 		_handler.update(tick);
 
 		return true;
 	}
 
+	bool helperFunction() {
+		if (targetcommandList.size() == commandList.size()) {
+			for (int i = 0; i < commandList.size(); ++i) {
+				if ((targetcommandList[i] != commandList[i]) || (targetbuttonList[i] != buttonList[i])) {
+					return false;
+				}
+			}
+			return true;
+		}
+		return false;
+	}
+
 private:
 	float m_fPlayerX;
 	float m_fPlayerY;
+	int input_Count;
+	std::vector<std::string> commandList = {};
+	std::vector<std::string> buttonList = {};
+	std::vector<std::string> targetcommandList = {"INPUT_236", "INPUT_623","INPUT_236", "INPUT_63214"};
+	std::vector<std::string> targetbuttonList = {"INPUT_PRESS_LP","INPUT_PRESS_HP","INPUT_PRESS_LP", "INPUT_PRESS_LP" };
 	InputHandler _handler = InputHandler();
 	MotionInput<1> _motions1[MOTIONS_1_SIZE];
 	MotionInput<5> _motions5[MOTIONS_1_SIZE];
